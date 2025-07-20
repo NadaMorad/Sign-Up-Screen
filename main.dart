d6edca6a15189a0267b670f4bccd6d6ba0a75731
Nada Morad <nadaelomda136@gmail.com>
2025-07-20 05:55:01 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/signup_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter SignUp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const SignUpScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
